d95771f65c988b33b0bd7137ee7c916b87e52190
AioaneiMirel <themyrel@yahoo.com>
2017-01-09 23:05:04 +0200

    aaas

--- test/Program.cs
+++ test/Program.cs
@@ -10,7 +10,7 @@ namespace test
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("This is a test!");
+            Console.WriteLine("This is a test!");     
         }
     }
 }
